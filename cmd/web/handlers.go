a300ab3e3abcfb51f1979d11eed05c9a461f1a72
chepaqq <chepaqq@protonmail.com>
2023-08-03 18:52:18 +0300

    feat: Add navigation bar

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -23,6 +23,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"./ui/html/base.html",
 		"./ui/html/pages/home.html",
+		"./ui/html/nav.html",
 	}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
