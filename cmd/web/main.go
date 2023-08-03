9c1db2ddd2c5e47059bc41dd51165f1cef430752
chepaqq <chepaqq@protonmail.com>
2023-08-03 17:55:13 +0300

    fix: Restrict the root url pattern

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,10 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
@@ -18,7 +22,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create a new snippet..."))
+	w.Write([]byte("Create a new snippet...")
 }
 
 func main() {
@@ -27,7 +31,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/snippet/create/", snippetCreate)
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
